4157d1509ddd057339e73330696445cfb8cd69bb
Emily Wisher <66205604+e-wisher@users.noreply.github.com>
2020-06-10 19:08:02 +0100

    Add files via upload

---
+++ embed.js
@@ -0,0 +1,19 @@
+new 
+
+embed = {
+    color: 0x0099ff,
+    title: 'some title',
+    description: 'some description',
+    url: 'https://github.com/e-wisher/FroggieBot',
+    author: {
+        name: 'name',
+        icon_url: 'https://imgur.com/gallery/PAxEQ6D',
+        url: 'https://github.com/e-wisher/FroggieBot'
+    },
+    fields: [
+        {
+            name: 'regular field',
+            value: 'some value'
+        }
+    ]
+}
---
+++ index.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const {prefix, token} = require('./config.json');
+
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
+
+//check for messages
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+}
+
+    //if true in file, and no args supplied: error
+    if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${message.author}!`;
+
+		if (command.usage) {
+			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+		}
+
+		return message.channel.send(reply);
+	}
+
+})
+
+
+client.login('token')
